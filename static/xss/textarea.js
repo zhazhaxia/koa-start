1efd62bd9d6ed34993cc27ba7712e2997c5a9948
zhazhaxia <337262356@qq.com>
2017-09-07 23:27:20 +0800

    xss demo

---
+++ static/xss/textarea.js
@@ -0,0 +1,12 @@
+function htmlEncode (str) {
+	var html = "";
+	var node = document.createElement('textarea');
+	if (node) {
+		node.innerText = str;
+		html =  node.innerHTML;
+		node = null;
+	}
+	return html;
+}
+
+alert(htmlEncode('<script>alert(0)</script>'))
